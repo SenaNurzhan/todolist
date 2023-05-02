61d9c71735dc687b0e9b6a48d86116067421b0a7
Sena <117431055+SenaNurzhan@users.noreply.github.com>
2023-05-02 21:55:52 +0600

    Add files via upload

--- assets/api/auth/login.php
+++ assets/api/auth/login.php
@@ -1,1 +1,19 @@
-
+<div class="login-wrapper" id="login">
+    <div class="login-form">
+        <div class="row">
+            <div class="col-xs-12 center">
+                <div class="form-wrap">
+                <h1>Login form</h1>
+                    <form role="form" action="?act=login" method="post" id="login-form" autocomplete="off">
+                        <input type="text" name="login" id="login" placeholder="Login" class="form-control">
+                        <input type="password" name="password" id="password" placeholder="Password" class="form-control">
+                        <div class="form-btns">
+                            <input type="submit" id="btn-login" class="login-btn" value="Login">
+                            <a href="?page=register"><button  type="button" class="regis-btn">Registration</button></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ assets/api/auth/register.php
@@ -0,0 +1,25 @@
+<div class="regis-wrapper" id="regis">
+    <div class="regis-form">
+        <div class="row">
+            <div class="col-xs-12 center">
+                <div class="regform-wrap">
+                <h1>Register form</h1>
+                    <form role="form" action="?act=register" method="post" id="regis-form" autocomplete="off">
+                        <input type="text" name="name" id="name" placeholder="Name" class="form-control">
+                        <input type="text" name="surname" id="surname" placeholder="Surname" class="form-control">
+                        <input type="text" name="login" id="login" placeholder="Login" class="form-control">
+                        <input type="text" name="email" id="email" placeholder="Email@xxx.com" class="form-control">
+                        <input type="number" name="age" id="age" placeholder="Age" class="form-control">
+                        <input type="password" name="password" id="password" placeholder="Password" class="form-control">
+                        <input type="password" name="conpassword" id="conpassword" placeholder="Confirm Password" class="form-control">
+                        <hr>
+                        <div class="regform-btns">
+                            <input type="submit" id="btn-reg" class="reg-btn" value="Register">
+                            <a href="?act=goback"><button  type="button" class="back-btn">Go back</button></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
